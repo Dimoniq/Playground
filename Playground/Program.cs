1822de1ccc04243b10dbd4dcc9a72fab9cbd3de2
Liakhovych Dmytro <dimoniq@gmail.com>
2020-12-09 21:50:38 +0100

    initial commit

---
+++ Playground/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Playground
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+      IConfiguration config = builder.Build();
+
+      var section = config.GetSection("Root");
+
+
+      var result =  config.GetSection("Root").Get<Composite<ServiceA>>();
+
+
+      IComposite<ServiceA> item =  result;
+
+      Console.WriteLine("end");
+    }
+  }
+
+
+  interface IComposite<TServiceInterface>
+  {
+    public string Commons { get; set; }
+    public TServiceInterface Custom { get; set; }
+  }
+
+  class Composite<TServiceInterface> : IComposite<TServiceInterface>
+  {
+    public string Commons { get; set; }
+    public TServiceInterface Custom { get; set; }
+  }
+
+  interface IServiceA
+  {
+    public string Name { get; set; }
+  }
+
+  class ServiceA : IServiceA
+  {
+    public string Name { get; set; }
+  }
+}
